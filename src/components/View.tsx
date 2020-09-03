900714036abddf08d9dca458abf0d9ad728bd829
Ran Elishayev <rannygmx@gmail.com>
2020-09-03 11:29:42 +0300

    infra(prettier)

--- src/components/View.tsx
+++ src/components/View.tsx
@@ -1,24 +1,20 @@
-import React from 'react';
-import {
-  Map,
-  TileLayer,
-  TileOsm,
-} from '@map-colonies/react-components';
+import React from "react";
+import { Map, TileLayer, TileOsm } from "@map-colonies/react-components";
 
 const styles = {
   map: {
-    width: '100%',
-    height: '100%',
-    position: 'fixed' as const
-  }
-}
+    width: "100%",
+    height: "100%",
+    position: "fixed" as const,
+  },
+};
 
 const View: React.FC = () => {
   return (
     <div style={styles.map}>
       <Map>
         <TileLayer>
-          <TileOsm/>
+          <TileOsm />
         </TileLayer>
       </Map>
     </div>
