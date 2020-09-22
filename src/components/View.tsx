0e67cdfe752f11fe8d03637133c268b8bd3c5f59
Ran Elishayev <42379604+rannyeli@users.noreply.github.com>
2020-09-22 10:31:33 +0300

    feat(tiles, Dockerfile): Added Dockerfile + support for configurable tile layers (#11)
    * feat(tiles): added wms and wmts support
    
    * feat: added Dockerfile
    
    * fix: lint
    
    * fix(dockerfile): use specific nginx version

--- src/components/View.tsx
+++ src/components/View.tsx
@@ -1,15 +1,18 @@
 import React, { useReducer } from 'react';
+import useAxios from 'axios-hooks';
 import { initialState } from '../layers';
 import { LayersProvider } from '../providers/LayersProvider';
 import { reducer } from '../reducer';
+import { Config } from '../model/configType';
 import Menu from './Menu';
 import Map from './Map';
 
 const View: React.FC = () => {
   const [layers, dispatch] = useReducer(reducer, initialState);
+  const [{ data, loading, error }, refetch] = useAxios<Config>('config.json');
   return (
     <LayersProvider value={[layers, dispatch]}>
-      <Map />
+      {!loading && <Map config={data} />}
       <Menu />
     </LayersProvider>
   );
